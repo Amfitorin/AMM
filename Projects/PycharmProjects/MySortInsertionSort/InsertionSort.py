f54394bf689c69735596fef13ebba8f9c374c81b
Hroniko <hroniko@bk.ru>
2016-09-28 17:06:22 +0300

    Доработана реализация сортировок на Python, не работают шейкер и Шелл

--- Projects/PycharmProjects/MySortInsertionSort/InsertionSort.py
+++ Projects/PycharmProjects/MySortInsertionSort/InsertionSort.py
@@ -34,7 +34,7 @@ def insertionSortBisection(arr):
             else:
                 R = M
         # j = i
-        for j in range(i, R, -1): # от i до R ВКЛЮЧИТЕЛЬНО!
+        for j in range(i, R, -1):  # от i до R ВКЛЮЧИТЕЛЬНО!
             arr[j] = arr[j - 1]
         arr[R] = tmp  # Помещаем текущий элемент на подходящее место
     return arr
@@ -59,19 +59,18 @@ def sort(arr, L, R):
             j = j - 1
         if (i > j):  # Выход из бесконечного цикла по условию
             break
-        if (L < j):
-            sort(arr, L, j)
-        if (i < R):
-            sort(arr, i, R)
+    if (L < j):
+        sort(arr, L, j)
+    if (i < R):
+        sort(arr, i, R)
     return arr
 
 
 # Начальная процедура для быстрой сортировки с рекурсией, инициирует рекурсию Sort
-def quickSort(arr1):
-    size = len(arr1)
+def quickSort(arr):
+    size = len(arr)
     # Запускаем сортировку массива:
-    sort(arr1, 1, size - 1)
-    return arr1
+    return sort(arr, 1, size - 1)
 
 
 # Метод быстрой сортировки без рекурсии:
@@ -107,14 +106,13 @@ def quickSortNonRecursive(arr):
                     j = j - 1
                 if (i > j):  # Выход из третьего бесконечного цикла
                     break
-                if (i < R):  # Сохранить в стеке запрс на сортировку правой части
-                    s = s + 1
-                    stackLow[s] = i
-                    stackHigh[s] = R
-                R = j
+            if (i < R):  # Сохранить в стеке запрс на сортировку правой части
+                s = s + 1
+                stackLow[s] = i
+                stackHigh[s] = R
+            R = j
             if (L >= R):  # Выход из второго бесконечного цикла
                 break  # Теперь L и R ограничивает левую
-
         if (s < 0):  # Выход из внешнего бесконечного цикла # while (s = -1)
             break
     return arr
@@ -136,11 +134,11 @@ def selectionSort(arr):
     return arr
 
 
-# Метод сортировки прямого обмена (Пузырьковая сортировка):
+# Метод сортировки gepshmrjv (прямого обмена):
 def bubbleSort(arr):
     size = len(arr)
     # Запускаем сортировку массива:
-    for i in range(2, size - 1):
+    for i in range(1, size - 1, +1): # по Вирту от 2го элемента вроде, но так не пошло
         for j in range(size - 1, i, -1):
             if (arr[j - 1] > arr[j]):
                 tmp = arr[j - 1]
@@ -154,8 +152,8 @@ def shakerSort(arr):
     size = len(arr)
     # Запускаем сортировку массива:
     L = 2
-    R = size - 1
-    k = size - 1
+    R = size-1
+    k = R
     while True:  # Запускаем внешний бесконечный цикл
         for j in range(R, L, -1):  # Проход справа налево
             if (arr[j - 1] > arr[j]):
@@ -164,15 +162,38 @@ def shakerSort(arr):
                 arr[j] = tmp
                 k = j
         L = k + 1  # Сдвигаем левую границу вправо
-        for j in range(L, R):  # Проход слева направо
+        for j in range(L, R, +1):  # Проход слева направо
             if (arr[j - 1] > arr[j]):
                 tmp = arr[j - 1]
                 arr[j - 1] = arr[j]
                 arr[j] = tmp
                 k = j
         R = k - 1  # Сдвигаем правую границу влево
-        if (L > R):  # Выходим из внешнего бесконечного цикла
+        if (L >= R):  # Выходим из внешнего бесконечного цикла
             break
+    return arr
+
+
+# Метод сортировки Шелла (несколько последовательных сортировок с разной дальностью):
+def shellSort(arr):
+    size = len(arr)
+    T = 4;  # Всего T расстояний сортировки
+    h = [9, 5, 3, 1]  # Массив расстояний сортировок
+    # Запускаем сортировку массива:
+    for m in range(0, T - 1):
+        k = h[m]
+        for i in range(k, size - 1):
+            tmp = arr[i]
+            j = i - k
+            while ((j >= k) and (tmp < arr[j])):
+                arr[j + k] = arr[j]
+                j = j - k
+
+            if not ((j >= k) and (tmp >= arr[j])):
+                arr[j + k] = tmp
+            else:
+                arr[j + k] = arr[j]
+                arr[j] = tmp
     return arr
 
 
@@ -186,27 +207,76 @@ def main():
     ar[0] = "Заглушка"
     print(ar)  # Выводим на экран
 
-    # Делаем копию массива для второго алгоритма
-    ar2 = copy.deepcopy(ar)
-
-    # Запускаем сортировку прямыми вставками:
-    ar1 = copy.deepcopy(ar)
+    # 1. Запускаем сортировку прямыми вставками:
+    ar1 = copy.deepcopy(ar)  # Делаем копию массива
     t = time.time()
-    insertionSort(ar1)
+    insertionSort(ar1)  # Непосредственная сортировка
     t = (time.time() - t) * 1000
-    print("Длительность сортировки прямыми вставками: {:.0f} мс".format(t))
+    print("Длительность сортировки прямыми вставками: {:.0f} мс".format(
+        t))  # {:.3f} мс".format(t)) # три знакка после точки
     print(ar1)  # Выводим на экран
 
-    # Запускаем сортировку прямыми вставками с бисекцией:
+    # 2. Запускаем сортировку прямыми вставками с бисекцией:
     ar1 = copy.deepcopy(ar)
     t = time.time()
-    insertionSortBisection(ar1)  # insertionSortBisection
+    insertionSortBisection(ar1)
     t = (time.time() - t) * 1000
     print("Длительность сортировки прямыми вставками с бисекцией: {:.0f} мс".format(t))
+    print(ar1)
+
+    # 3. Запускаем быструю сортировку с рекурсией:
+    ar1 = copy.deepcopy(ar)
+    t = time.time()
+    quickSort(ar1)
+    t = (time.time() - t) * 1000
+    print("Длительность быстрой сортировки с рекурсией: {:.0f} мс".format(t))
+    print(ar1)
+
+    # 4. Запускаем быструю сортировку без рекурсии:
+    ar1 = copy.deepcopy(ar)
+    t = time.time()
+    quickSortNonRecursive(ar1)
+    t = (time.time() - t) * 1000
+    print("Длительность быстрой сортировки без рекурсии: {:.0f} мс".format(t))
+    print(ar1)
+
+    # 5. Запускаем сортировку прямым выбором:
+    ar1 = copy.deepcopy(ar)
+    t = time.time()
+    selectionSort(ar1)
+    t = (time.time() - t) * 1000
+    print("Длительность сортировки прямым выбором: {:.0f} мс".format(t))
+    print(ar1)
+
+    # 6. Запускаем сортировку пузырьком:
+    ar1 = copy.deepcopy(ar)
+    t = time.time()
+    bubbleSort(ar1)
+    t = (time.time() - t) * 1000
+    print("Длительность сортировки пузырьком: {:.0f} мс".format(t))
+    print(ar1)
+
+    # 7. Запускаем шейкерную сортировку:
+    ar1 = copy.deepcopy(ar)
+    t = time.time()
+    shakerSort(ar1)
+    t = (time.time() - t) * 1000
+    print("Длительность шейкерной сортировки: {:.0f} мс".format(t))
+    print(ar1)
+
+    # 8. Зарускаем сортировку Шелла:
+    ar1 = copy.deepcopy(ar)
+    t = time.time()
+    shellSort(ar1)
+    t = (time.time() - t) * 1000
+    print("Длительность сортировки Шелла: {:.0f} мс".format(t))
     print(ar1)
 
     return 0
 
 
+# Окончание основной программы
+
+# Метод (точка) входа
 if __name__ == "__main__":
     main()
